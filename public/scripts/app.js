1e5b7ec69b55ef6c4c76ba44cecf8e574f271e03
Isaiah Mutekanga <isaiahmutekanga@trentu.ca>
2021-11-29 22:05:05 +0000

    quizzes-new and some js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,25 @@
 // Client facing scripts here
 $(document).ready(function () {
-  $("#nav-placeholder").load("views/nav.html");
+  $(".addquestion").prepend(`<section class="newquiz">
+        <form action="">
+          <label for="QuizTitle">Quiz Title:</label>
+          <input type="text" />
+          <br />
+          <label for="question">Question</label>
+          <input type="text" />
+          <br />
+          <label for="question">Option 1</label>
+          <input type="text" />
+          <br />
+          <label for="question">Option 2</label>
+          <input type="text" />
+          <br />
+          <label for="question">Option 3</label>
+          <input type="text" />
+          <br />
+          <label for="question">Option 4</label>
+          <input type="text" />
+          <br />
+        </form>
+      </section>`);
 });
