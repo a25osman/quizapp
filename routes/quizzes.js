fce4b781f9a572eae7f616f7a2feb029687392af
Awab Osman <awabosman3@gmail.com>
2021-11-30 17:30:34 -0500

    quiz js file

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -42,55 +42,43 @@ module.exports = (db) => {
 
   // POST /quizzes/
   router.post("/new", (req, res) => {
-    db.query(`INSERT INTO quizzes (title) VALUES ($1);`, [req.body.title])
-      .then((data) => {
-        data.rows[0];
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-
+    // let userId = req.session.userId;
     let numberOfQuestions = (Object.keys(req.body).length - 1) / 3;
-
-    for (let i = 0; i < numberOfQuestions; i++) {
-      let str_question = `question${i}`;
-      let str_answer = `answer${i}`;
-      let str_options = `options${i}`;
-      let question_id;
-      db.query(`INSERT INTO questions (content) VALUES ($1) RETURNING *;`, [
-        req.body.str_question[0],
-      ])
-        .then((data) => {
-          question_id = data.rows[0].id;
-        })
-        .catch((err) => {
-          console.log(err.message);
-        });
-      for (let j in req.body.str_answer) {
-        if (j === Number(req.body.str_options[0])) {
-          db.query(
-            `INSERT INTO answers (content, is_correct) VALUES ($1, TRUE);`,
-            [req.body.str_answer[j]]
-          )
+    let userID = 1;
+    let quizID;
+    db.query(`INSERT INTO quizzes (user_id, title) VALUES ($1, $2) RETURNING *;`, [userID, req.body.title])
+      .then((data) => {
+        quizID = data.rows[0].id;
+        for (let i = 0; i < numberOfQuestions; i++) {
+          let str_question = `question${i}`;
+          let str_answer = `answer${i}`;
+          let str_options = `options${i}`;
+          let questionID;
+          db.query(`INSERT INTO questions (quiz_id, content) VALUES ($1, $2) RETURNING *;`, [quizID, req.body[str_question][0]])
             .then((data) => {
-              console.log(data.rows[0]);
+              questionID = data.rows[0].id;
+              for (let j in req.body[str_answer]) {
+                if (j === Number(req.body[str_options][0])) {
+                  db.query(
+                    `INSERT INTO answers (question_id, content, is_correct) VALUES ($1, $2, true) RETURNING *;`,
+                    [questionID, req.body[str_answer][j]]
+                  )
+                    .then(res.redirect('/'))
+                    .catch((err) => console.log(err.message));
+                } else {
+                  db.query(
+                    `INSERT INTO answers (question_id, content) VALUES ($1, $2) RETURNING *;`,
+                    [questionID, req.body[str_answer][j]]
+                  )
+                    .then(res.redirect('/'))
+                    .catch((err) => console.log(err.message));
+                }
+              }
             })
-            .catch((err) => {
-              console.log(err.message);
-            });
-        } else {
-          db.query(`INSERT INTO answers (content) VALUES ($1);`, [
-            req.body.str_answer[j],
-          ])
-            .then((data) => {
-              data.rows[0];
-            })
-            .catch((err) => {
-              console.log(err.message);
-            });
+            .catch((err) => console.log(err.message));
         }
-      }
-    }
+      })
+      .catch((err) => console.log(err.message));
   });
 
   return router;
