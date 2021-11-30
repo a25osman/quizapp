8aea8b772d0454a4b816abfe7fc1fe8d253697c9
Isaiah Mutekanga <isaiahmutekanga@trentu.ca>
2021-11-30 19:52:31 +0000

    No changes

--- routes/quizzes.js
+++ routes/quizzes.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 module.exports = (db) => {
-
   // GET /quizzes/quiz/:quiz_id
   router.get("/quiz/:quiz_id", (req, res) => {
-    db.query(`
+    db.query(
+      `
     SELECT privacy, title, questions.id, questions.content, answers.id, answers.content
     FROM quizzes
     JOIN questions ON quizzes.id = quiz_id
     JOIN answers ON questions.id = question_id
     WHERE quizzes.id = $1
-    ;`, [req.params.quiz_id])
-    .then(data => {
-      const quiz = data.rows[0];
-      let templateVars = {quiz};
-      res.render("quiz_show", templateVars); // quiz is an object containing quiz questions and answers
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
+    ;`,
+      [req.params.quiz_id]
+    )
+      .then((data) => {
+        const quiz = data.rows[0];
+        let templateVars = { quiz };
+        res.render("quiz_show", templateVars); // quiz is an object containing quiz questions and answers
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
   });
 
   // GET /quizzes/new
@@ -37,46 +37,57 @@ module.exports = (db) => {
     // } else {
     //   res.redirect("/login")
     // }
-    res.render("quizzes_new")
+    res.render("quizzes_new");
   });
 
   // do we need returning* below??
-  router.post("/",(req, res) =>{
+  router.post("/", (req, res) => {
     db.query(`INSERT INTO quizzes (title) VALUES ($1);`, [req.body.title])
-    .then(data => {
-      data.rows[0]
-    }).catch((err)=>{
-      console.log(err.message);
-    })
+      .then((data) => {
+        data.rows[0];
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
 
     let numberOfQuestions = (Object.keys(req.body).length - 1) / 3;
 
-    for (let i = 0; i < numberOfQuestions; i ++) {
+    for (let i = 0; i < numberOfQuestions; i++) {
       let str_question = `question${i}`;
       let str_answer = `answer${i}`;
       let str_options = `options${i}`;
       let question_id;
-      db.query(`INSERT INTO questions (content) VALUES ($1) RETURNING *;`, [req.body.str_question[0]])
-        .then(data => {
-          question_id = data.rows[0].id
-        }).catch((err)=>{
-          console.log(err.message);
+      db.query(`INSERT INTO questions (content) VALUES ($1) RETURNING *;`, [
+        req.body.str_question[0],
+      ])
+        .then((data) => {
+          question_id = data.rows[0].id;
         })
+        .catch((err) => {
+          console.log(err.message);
+        });
       for (let j in req.body.str_answer) {
-        if (j === Number(req.body.str_options[0])){
-          db.query(`INSERT INTO answers (content, is_correct) VALUES ($1, TRUE);`, [req.body.str_answer[j]])
-          .then(data => {
-            console.log(data.rows[0]);
-          }).catch((err)=>{
-            console.log(err.message);
-          })
+        if (j === Number(req.body.str_options[0])) {
+          db.query(
+            `INSERT INTO answers (content, is_correct) VALUES ($1, TRUE);`,
+            [req.body.str_answer[j]]
+          )
+            .then((data) => {
+              console.log(data.rows[0]);
+            })
+            .catch((err) => {
+              console.log(err.message);
+            });
         } else {
-          db.query(`INSERT INTO answers (content) VALUES ($1);`, [req.body.str_answer[j]])
-          .then(data => {
-            data.rows[0]
-          }).catch((err)=>{
-            console.log(err.message);
-          })
+          db.query(`INSERT INTO answers (content) VALUES ($1);`, [
+            req.body.str_answer[j],
+          ])
+            .then((data) => {
+              data.rows[0];
+            })
+            .catch((err) => {
+              console.log(err.message);
+            });
         }
       }
     }
