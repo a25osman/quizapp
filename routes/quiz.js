e7d8a25bce1db3cdcf487c67f99fe7ef7ceb2ce3
Awab Osman <awabosman3@gmail.com>
2021-12-01 17:16:44 -0500

    quiz  route with get function

--- routes/quiz.js
+++ routes/quiz.js
@@ -5,7 +5,7 @@ module.exports = (db) => {
   // GET /quiz/:quiz_id
   router.get("/:quiz_id", (req, res) => {
     db.query(`
-      SELECT title, questions.id AS question_id, questions.content AS question,
+      SELECT title, quizzes.id as quiz_id, questions.id AS question_id, questions.content AS question,
         answers.id AS ans_id, answers.content AS option, answers.is_correct AS is_correct
       FROM quizzes
       JOIN questions ON quizzes.id = quiz_id
@@ -16,10 +16,36 @@ module.exports = (db) => {
       [req.params.quiz_id]
     )
       .then((data) => {
-        console.log(data.rows)
-        const quiz = data.rows[0];
-        
-        let templateVars = {quiz, userInfo: req.session.user}; 
+        // console.log(data.rows)
+        console.log("refresh succesful")
+        const array = data.rows;
+        let numRuns = 0;
+        let numQuestions = array.length/4;
+        let quiz = [];
+        while (numRuns < numQuestions) {
+          let quizQuestion = {
+            title: null,
+            quiz_id: null,
+            question_id: null,
+            question: null,
+            ans_id: [],
+            option: [],
+            is_correct: []
+          };
+          for (let i = 0 + 4*numRuns; i < 4 + 4*numRuns; i++) {
+            quizQuestion.title = array[i].title;
+            quizQuestion.quiz_id = array[i].quiz_id;
+            quizQuestion.question_id = array[i].question_id;
+            quizQuestion.question = array[i].question;
+            quizQuestion.ans_id.push(array[i].ans_id);
+            quizQuestion.option.push(array[i].option);
+            quizQuestion.is_correct.push(array[i].is_correct);
+          }
+          quiz.push(quizQuestion)
+          numRuns ++;
+        }
+        let templateVars = {quiz: quiz, userInfo: req.session.user};
+        console.log("---TEMPLATE VARS RETURNS QUIZ-\n\n\n", quiz)
         res.render("quiz_page", templateVars); // user is an object which contains user info
       })
       .catch((err) => {
