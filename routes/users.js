2e5e05443041293b01d85e3b8fbeb73b995d37d7
amoham1 <amoham456@gmail.com>
2021-12-01 14:17:41 -0500

    created the get for user profile page

--- routes/users.js
+++ routes/users.js
@@ -9,12 +9,30 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = (db) => {
-  // GET /profile/:user_id
+  // GET /users/:user_id
+  router.get("/creator/:user_id", (req, res) => {
+    const userid = req.session.userid;
+    if (!userid){
+      return res.redirect("/");
+    }
+
+    db.query(`SELECT * FROM quizzes WHERE user_id = $1;`, [userid])
+    .then(data => {
+      console.log("-------",data.rows)
+      console.log("------",req.session.username)
+      let templateVars = {quizzes: data.rows, userInfo: req.session.user};
+      res.render("profile_user", templateVars);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+  });
+
   router.get("/:user_id", (req, res) => {
 
   });
 
-  // GET /profile/:user_id/:quiz_id  --> Quiz Result Page
+  // GET /users/:user_id/:quiz_id  --> Quiz Result Page
   router.get("/:user_id/:quiz_id", (req, res) => {
 
   });
