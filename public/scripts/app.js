03ebc01eee4d27c68a8a85005040365fb282efda
Isaiah Mutekanga <isaiahmutekanga@trentu.ca>
2021-12-03 14:54:42 +0000

    Final changes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -26,41 +26,46 @@ const createElement = function (questionNumber) {
           <option value="2">Option C</option>
           <option value="3">Option D</option>
         </select>
-<button class="deletequestion">Remove Question</button>
+
       </div>
       `;
 };
 
 $(document).ready(function () {
   const deletequestion = ".deletequestion";
+
   $(".addquestion").on("click", function () {
     const questionNumber = $(".question").length;
 
     $("#myform").append(createElement(questionNumber));
-
-    $(".deletequestion").on("click", function (e) {
-      e.preventDefault();
-      //debugger;
-      e.target.parentElement.childNodes[3].value = "";
-      e.target.parentElement.childNodes[9].value = "";
-      e.target.parentElement.childNodes[15].value = "";
-      e.target.parentElement.childNodes[21].value = "";
-      e.target.parentElement.childNodes[27].value = "";
-      e.target.parentElement.remove();
-      $(".question").each((i, element) => {
-        console.log(element, i);
-        element.setAttribute("class", `question question-${i}`);
-        element.firstElementChild.innerText = `Question #${1 + i}`;
-        element.children[1].name = `question${i}[]`;
-        element.children[4].name = `choices${i}[]`;
-        element.children[7].name = `choices${i}[]`;
-        element.children[10].name = `choices${i}[]`;
-        element.children[13].name = `choices${i}[]`;
-        element.children[16].name = `choices${i}[]`;
-      });
-      return false;
-    });
   });
+  $(".deletequestion").on("click", () => {
+    const questionNumber = $(".question").length;
+    $(`.question-${questionNumber - 1}`).remove();
+  });
+  // $(".deletequestion").on("click", function (e) {
+  //   e.preventDefault();
+  //   //debugger;
+  //   e.target.parentElement.childNodes[3].value = "";
+  //   e.target.parentElement.childNodes[9].value = "";
+  //   e.target.parentElement.childNodes[15].value = "";
+  //   e.target.parentElement.childNodes[21].value = "";
+  //   e.target.parentElement.childNodes[27].value = "";
+  //   e.target.parentElement.remove();
+  //   $(".question").each((i, element) => {
+  //     console.log(element, i);
+  //     element.setAttribute("class", `question question-${i}`);
+  //     element.firstElementChild.innerText = `Question #${1 + i}`;
+  //     element.children[1].name = `question${i}[]`;
+  //     element.children[4].name = `choices${i}[]`;
+  //     element.children[7].name = `choices${i}[]`;
+  //     element.children[10].name = `choices${i}[]`;
+  //     element.children[13].name = `choices${i}[]`;
+  //     element.children[16].name = `choices${i}[]`;
+  //   });
+  //   return false;
+  // });
+  // });
   $(".postquiz").on("click", function (e) {
     var x = document.forms["myForm"]["fname"].value;
     if (x == "") {
